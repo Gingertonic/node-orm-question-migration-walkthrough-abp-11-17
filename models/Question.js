02390b494e5ac7e2cc8a91730a37fe9e062210b3
Beth Schofield <bethmschofield@gmail.com>
2017-11-21 22:32:55 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -6,7 +6,7 @@ class Question{
  static CreateTable() {
   //  sql.CREATETABLE
    return new Promise (function(resolve){
-     sql.CREATE TABLE questions (id INTEGER PRIMARY KEY, content TEXT);
+     sql CREATE TABLE questions (id INTEGER PRIMARY KEY, content TEXT);
    resolve ("Kept my promise!")
   })
  }
